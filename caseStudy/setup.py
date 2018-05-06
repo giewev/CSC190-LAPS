e64d51de551d9032c3d5451c63345a2676e5b92e
Naresh Kc <gypsysoulza@gmail.com>
2018-05-05 20:03:10 -0400

    edit

--- caseStudy/setup.py
+++ caseStudy/setup.py
@@ -1,3 +1,3 @@
 import os;
-cmd = "sshpass -p 'goodyear123!@#' ssh-t root@169.254.236.100 sudo service apache2 stop";
+cmd = "sshpass -p 'goodyear123!@#' ssh-t root@169.254.236.101 sudo service apache2 stop";
 os.system(cmd);
