0ea0da27208777a1ba47a8368a306c87f2207340
Naresh Kc <gypsysoulza@gmail.com>
2018-05-11 21:46:47 +0000

    Update setupDb.sql
    edit

--- src/ScoreBoard/setupDb.sql
+++ src/ScoreBoard/setupDb.sql
@@ -3,7 +3,17 @@ use laps;
 
 drop table if exists users;
 create table users
-	(id int,
+	( qid INT
 	 name varchar(256),
 	 pass varchar(256));
 
+
+CREATE TABLE tbl_scores(
+	uname varchar(255),
+	qid INT,
+	score INT
+);
+
+INSERT INTO tbl_users (name, qid, score) VALUES ('John', '1', 'apple1!');
+INSERT INTO tbl_users (name, qid, score) VALUES ('Johny', '2', 'orange1!');
+INSERT INTO tbl_scores (uname, qid, score) VALUES ('Johnson', '4', '10');
