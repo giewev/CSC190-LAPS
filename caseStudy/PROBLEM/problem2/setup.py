7dd4903721587a803b27e589ba2be74b6da2f685
Naresh Kc <gypsysoulza@gmail.com>
2018-05-11 20:02:06 -0400

    Changes made

--- caseStudy/PROBLEM/problem2/setup.py
+++ caseStudy/PROBLEM/problem2/setup.py
@@ -1,3 +1,3 @@
 import os;
-cmd = "sshpass -p 'goodyear123!@#' ssh-t root@169.254.236.101 sudo touch virus.html";
+cmd = "sshpass -p 'goodyear123!@#' ssh-t root@169.254.236.101 sudo touch /var/www/html/virus.html";
 os.system(cmd);
