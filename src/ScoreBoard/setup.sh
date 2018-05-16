8d4294c478733aaa062d87e6649067425ce18b07
Ian Fade <giewev@gmail.com>
2018-05-15 20:38:59 -0400

    Add tarring of problem files to the setup script

--- src/ScoreBoard/setup.sh
+++ src/ScoreBoard/setup.sh
@@ -1,3 +1,12 @@
+rm -r /var/www/html/*
+
+cp -r Problems/ /var/www/html/Problems
 cp ./*.php /var/www/html/
 chown -R apache:apache /var/www/html
 mysql -u root -pgoodyear123!@# <setupDb.sql
+
+cd /var/www/html/Problems/
+for i in *
+do
+    tar czf $i.tar $i
+done
