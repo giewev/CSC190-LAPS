ea5880800e1cd969c3a10ab84e810b9ccfada866
Ian Fade <giewev@gmail.com>
2018-05-15 20:10:54 -0400

    Move problem resources to a standardized directory

--- src/ScoreBoard/challenges/deactivate_apache/grade.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-request = requests.get("169.254.236.101")
-if "Apache" in request.headers['server']:
-    print "10"
-else: 
-    print "0"
--- src/ScoreBoard/challenges/deactivate_apache/setup.py
+++
@@ -1,3 +0,0 @@
-import os;
-cmd = "sshpass -p 'goodyear123!@#' ssh-t root@169.254.236.100 sudo service apache2 stop";
-os.system(cmd);
